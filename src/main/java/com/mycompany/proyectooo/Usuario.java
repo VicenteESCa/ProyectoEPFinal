7f940244d1ad558546c16d88b46af7a7941eaa6f
Andrea Pérez <120620899+Andy-ydnaa@users.noreply.github.com>
2023-11-29 19:28:59 -0300

    Update Usuario.java
    Listas para proveedor y consumidor, y su metodo de agregacion

--- src/main/java/com/mycompany/proyectooo/Usuario.java
+++ src/main/java/com/mycompany/proyectooo/Usuario.java
@@ -1,120 +1,86 @@
 package com.mycompany.proyectooo;
 
-import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-/**
- * @author Francisco
- */
+
 public class Usuario {
+    private static Scanner scanner = new Scanner(System.in);
+    private static List<Proveedor> proveedores = new ArrayList<>();
+    private static List<Consumidor> consumidores = new ArrayList<>();
+
     private String nombre;
-    private String rut;
     private String comuna;
     private String region;
-    private String direccion;
-    private String contraseña;
-    private static ArrayList<Usuario>listaUsuario = new ArrayList<Usuario>();
-    public Scanner entrada = new Scanner(System.in);
+
     
-    public Usuario(){
-        this.nombre = "";
-        this.rut = "";
-        this.comuna = "";
-        this.region = "";
-        this.direccion = "";
-         this.contraseña = "";
-    }
-    
-    public Usuario(String nombre, String rut, String comuna,String region, String direccion,String contraseña){
+    public Usuario(String nombre, String comuna, String region){
         this.nombre = nombre;
-        this.rut = rut;
         this.comuna = comuna;
         this.region = region;
-        this.direccion = direccion;
-        this.contraseña = contraseña;
-        
+    }
+ 
+    public String getNombre() {
+        return nombre;
     }
 
-    
-    public String getContraseña() {
-        return contraseña;
-    }
-    public String getNombre(){
-        return this.nombre;
-    }
-    public String getRut(){
-        return this.rut;
-    }
-    public String getComuna(){
-        return this.comuna;
-    }
-    public String getRegion(){
-        return this.region;
-    }
-    public String getDireccion(){
-        return this.direccion;
-    }
-
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
-    }
-    
-    public void setNombre(String nombre)
-    {
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    public void setRut(String rut)
-    {
-        this.rut = rut;
+    public String getComuna() {
+        return comuna;
     }
-    public void setComuna(String comuna)
-    {
+
+    public void setComuna(String comuna) {
         this.comuna = comuna;
     }
-    public void setRegion(String region)
-    {
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
         this.region = region;
     }
-    public void setDireccion(String direccion)
-    {
-        this.direccion = direccion;
-    }
-
-    public static ArrayList<Usuario> obtenerListaUsuarios() {
-        return listaUsuario;
-    }
-    
-    public boolean crearDato() throws ParseException{
-        System.out.println("Ingrese Nombre de Usuario");
-        this.nombre = entrada.next();
-        System.out.println("Ingrese Rut");
-        this.rut = entrada.next();
-        System.out.println("Ingrese Comuna");
-        this.comuna = entrada.next();
-        System.out.println("Ingrese Region ");
-        this.region = entrada.next();
-        System.out.println("Ingrese Direccion");
-        this.direccion = entrada.next();
-        System.out.println("Ingrese Contraseña");
-        this.contraseña=entrada.next();
+     public static void agregarProveedor() {
+        System.out.println("Ingrese los datos del Proveedor:");
+        System.out.print("Nombre: ");
+        String nombre = scanner.nextLine();
         
-        listaUsuario.add(new Usuario(this.nombre,this.rut,this.comuna,this.region,this.direccion,this.contraseña));
+        System.out.print("Comuna: ");
+        String comuna = scanner.nextLine();
         
-        return true;
-    }
+        System.out.print("Región: ");
+        String region = scanner.nextLine();
 
-    //modificaciones entrega 2
-    /*
-    @Override
-    public String toString() {
-    return "Nombre: " + nombreProducto + "\n" +
-           "Código: " + id + "\n" +
-           "Tipo: " + (tipoProducto == 1 ? "Reposteria" : "Aseo") + "\n" +
-           "Precio: $" + precioProducto + "\n" +
-           "Stock disponible: " + stockProducto + "\n";
+        scanner.nextLine(); // Consumir línea restante
+        System.out.print("Dirección Proveedor: ");
+        String direccion = scanner.nextLine();
+
+        Proveedor nuevoProveedor = new Proveedor(nombre, comuna, region, direccion);
+        proveedores.add(nuevoProveedor);
     }
-*/
+     public static void agregarConsumidor() {
+        System.out.println("Ingrese los datos del Consumidor:");
+        System.out.print("Nombre: ");
+        String nombre = scanner.nextLine();
+        
+        System.out.println("Rut: ");
+        String rut = scanner.nextLine();
+        
+        System.out.print("Comuna: ");
+        String comuna = scanner.nextLine();
+        
+        System.out.print("Región: ");
+        String region = scanner.nextLine();
+        
+        scanner.nextLine(); // Consumir línea restante
+        System.out.print("Dirección Consumidor: ");
+        String direccion = scanner.nextLine();
+
+        Consumidor nuevoConsumidor = new Consumidor(nombre, rut, comuna, region, direccion);
+        consumidores.add(nuevoConsumidor);
+    }
 }
-
